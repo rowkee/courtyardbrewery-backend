867175dba0de2fded9587073e33c28a75d1a2a37
rowkee <rick.keenan10@gmail.com>
2023-11-21 10:38:08 +0000

    test commit

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Beer, Merch, Order, Review, Rating
 
 # Register your models here.
+admin.site.register(Beer)
+admin.site.register(Merch)
+admin.site.register(Order)
+admin.site.register(Review)
+admin.site.register(Rating)
